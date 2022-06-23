def7aa99ef02f8d31601943a2f28b3289077095d
azamatdoronov <aza3el@inbox.ru>
2022-06-23 13:07:52 +0600

    Made index.html in templates

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from webapp.models import Task, STATUS_CHOICES
+
+
+def index_view(request):
+    tasks = Task.objects.order_by("-created_task")
+    context = {"tasks": tasks}
+    return render(request, "index.html", context)
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "create_task.html", {"statuses": STATUS_CHOICES})
+    else:
+        description = request.POST.get("description")
+        dead_line = request.POST.get("dead_line")
+        status = request.POST.get("status")
+        new_task = Task.objects.create(description=description, dead_line=dead_line, status=status)
+        context = {"task": new_task}
+        return render(request, "article_view.html", context)
