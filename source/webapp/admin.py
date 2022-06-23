a9d402bff0b151657c6bd21fa283ee70d63a694b
azamatdoronov <aza3el@inbox.ru>
2022-06-23 12:05:41 +0600

    Made changes in settings.py - TimeZone

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'created_task', 'dead_line', 'status']
+    list_display_links = ['id', 'description']
+    list_filter = ['status']
+    search_fields = ['description', 'dead_line']
+    fields = ['description', 'created_task', 'dead_line', 'status']
+    readonly_fields = ['description', 'created_task']
+
+
+admin.site.register(Task, TaskAdmin)
