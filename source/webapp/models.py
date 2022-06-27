b6aa5072fbed2af53a28fc05cb2f62fde3f1aba8
azamatdoronov <aza3el@inbox.ru>
2022-06-27 15:11:58 +0600

    Made new migration, add new column in table

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,12 +6,12 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class Task(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, default="No Title", verbose_name="Заголовок")
     description = models.TextField(max_length=1000, null=False, blank=False, verbose_name="Описание")
     created_task = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     dead_line = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0],
                               verbose_name="Статус")
-    title = models.CharField(max_length=50, null=False, blank=False, default="No Title", verbose_name="Заголовок")
 
     def __str__(self):
         return f"{self.id}. {self.description}: {self.status} {self.dead_line}"
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 
 from webapp.models import Task, STATUS_CHOICES
@@ -11,9 +10,9 @@ def index_view(request):
     return render(request, "index.html", context)
 
 
-def task_view(request):
-    pk = request.GET.get("pk")
-    task = Task.objects.get(pk=pk)
+def task_view(request, **kwargs):
+    pk = kwargs.get("pk")
+    task = get_object_or_404(Task, pk=pk)
     return render(request, "task_view.html", {"task": task})
 
 
@@ -21,15 +20,18 @@ def add_task(request):
     if request.method == "GET":
         return render(request, "add_task.html", {"statuses": STATUS_CHOICES})
     else:
+        title = request.POST.get("title")
         description = request.POST.get("description")
         status = request.POST.get("status")
         dead_line = request.POST.get("dead_line")
         if dead_line == '':
             dead_line = None
-            new_task = Task.objects.create(description=description, dead_line=dead_line, status=status)
-            context = {"task": new_task}
-            return render(request, 'task_view.html', context)
+            new_task = Task.objects.create(title=title, description=description, dead_line=dead_line, status=status)
+            # context = {"task": new_task}
+            # return render(request, "task_view.html", context)
+            return redirect("task_view.html", pk=new_task.pk)
         else:
-            new_task = Task.objects.create(description=description, dead_line=dead_line, status=status)
-            context = {"task": new_task}
-            return render(request, 'task_view.html', context)
+            new_task = Task.objects.create(title=title, description=description, dead_line=dead_line, status=status)
+            # context = {"task": new_task}
+            # return render(request, "task_view.html", context)
+            return redirect("task_view.html", pk=new_task.pk)
